2a8127fb9d5eaa59073fbe30741fa0991142f369
Samad126 <samed192005@gmail.com>
2025-06-17 15:48:04 +0400

    enhance accessibility by adding aria-labels to links and images in DynamicSection component

--- src/components/Homepage/dynamicsection/DynamicSection.jsx
+++ src/components/Homepage/dynamicsection/DynamicSection.jsx
@@ -21,15 +21,24 @@ function DynamicSection({ header, desc, sectionLink, links, video, img1, img2, i
 
   return (
     <section className="dynamicSection">
-      <Link id="mobileImageLink" to={sectionLink} onClick={() => (dispatch(resetImportantFilter()))}>
+      <Link
+        id="mobileImageLink"
+        to={sectionLink}
+        onClick={() => (dispatch(resetImportantFilter()))}
+        aria-label={isDynamic ? "Video link for " + header : "Image link for " + header}
+      >
         {isDynamic ?
-          <video ref={videoRef} controls={false} autoPlay src={video} muted loop></video> :
-          <img src={img1} alt="" />
+          <video ref={videoRef} controls={false} autoPlay src={video} muted loop aria-label={header}></video> :
+          <img src={img1} alt={header + " image"} />
         }
       </Link>
       <div id="desktopImageLink" className={!isDynamic ? "littleImageLink" : ""}>
-        <Link to={sectionLink} onClick={() => (dispatch(resetImportantFilter()))}>
-          <img src={img2} alt="" />
+        <Link
+          to={sectionLink}
+          onClick={() => (dispatch(resetImportantFilter()))}
+          aria-label={"Image link for " + header}
+        >
+          <img src={img2} alt={header + " image"} />
         </Link>
       </div>
       <div className="sectionMain">
@@ -37,11 +46,15 @@ function DynamicSection({ header, desc, sectionLink, links, video, img1, img2, i
         <p>{desc}</p>
         {secondTitle && <h3 className="whiteTxt">{secondTitle}</h3>}
         <div id="sectionCenterLinks">
-          <Link to={sectionLink} onClick={() => (dispatch(resetImportantFilter()))} className="whiteTxt bordered mx-auto">{buttonText}</Link>
+          <Link to={sectionLink} onClick={() => (dispatch(resetImportantFilter()))} className="whiteTxt bordered mx-auto" aria-label={buttonText}>
+            {buttonText}
+          </Link>
         </div>
       </div>
       <div id="sectionBottomLinks">
-        <Link to={sectionLink} onClick={() => (dispatch(resetImportantFilter()))} className="whiteTxt bordered">{buttonText}</Link>
+        <Link to={sectionLink} onClick={() => (dispatch(resetImportantFilter()))} className="whiteTxt bordered" aria-label={buttonText}>
+          {buttonText}
+        </Link>
       </div>
       {video && (
         <button id="toggler" onClick={handlePlay} aria-label={isPlaying ? "Pause video" : "Play video"}>
